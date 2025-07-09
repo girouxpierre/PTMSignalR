ff13a813b8e6a2945e6cebb50a50303a7cb8832b
girouxpierre <girouxpierre3@gmail.com>
2025-07-09 14:39:41 +0200

    Add files via upload

--- R/data.R
+++ R/data.R
@@ -1,12 +1,29 @@
 #' Clear cell renal cell carcinoma proteome and phosphoproteome
 #'
-#' Two dataset containing expression and phosphorylation values of CCRCC and
-#' two datasets containing immune and clinical information
+#' Dataset containing expression values of CCRCC
 #'   
 #'
-#' @format Data frames with 5998 rows and 66 variables, 6199 rows and 68 
-#' variables, 66 rows and 2 variables and 66 rows and 41 variables
+#' @format Data frame with 5998 rows and 66 variables
 #' @source Zhang & al., Nature Communications, 2023;
-#' @usage data(ccrcc)
-"ccrcc"
+#' @usage data(ccrcc_prot)
+"ccrcc_prot"
 
+#' Clear cell renal cell carcinoma phosphoproteome
+#'
+#' Dataset containing phosphorylation values of CCRCC
+#'   
+#'
+#' @format Data frame with 6199 rows and 68 variables
+#' @source Zhang & al., Nature Communications, 2023;
+#' @usage data(ccrcc_phos)
+"ccrcc_phos"
+
+#' Clear cell renal cell carcinoma clinical information
+#'
+#' Dataset containing clinical information
+#'   
+#'
+#' @format Data frame 66 rows and 41 variables
+#' @source Zhang & al., Nature Communications, 2023;
+#' @usage data(ccrcc_clinical)
+"ccrcc_clinical"
